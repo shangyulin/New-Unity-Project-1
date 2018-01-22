2ec79d59fbe4c597fd08c54f480b7c772c24673a
shangyulin <244049619@qq.com>
2018-01-22 17:22:30 +0800

    commit

---
+++ Assets/Scripts/PhotonCtrl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonCtrl : MonoBehaviour {
+
+    private const string roomName = "RoomName";
+    private RoomInfo[] roomsList;
+
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+    void OnGUI()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else if (PhotonNetwork.room == null)
+        {
+            // Create Room
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                PhotonNetwork.CreateRoom(roomName + Guid.NewGuid().ToString("N"));//, true, true, 5);
+
+            // Join Room
+            if (roomsList != null)
+            {
+                for(int i = 0; i < roomsList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+                        PhotonNetwork.JoinRoom(roomsList[i].name);
+                }
+            }
+        }
+    }
+
+    void OnReceivedRoomListUpdate()
+    {
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+    void OnJoinedRoom()
+    {
+        Debug.Log("Connected to Room");
+
+
+        PhotonNetwork.Instantiate("Player", new Vector3(0, 0,0), Quaternion.identity,0,null);
+    }
+}
---
+++ Assets/Scripts/player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Transform tr;
+    private PhotonView pv;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        pv = GetComponent<PhotonView>();
+    }
+	// Update is called once per frame
+	void Update () {
+        if(pv.isMine)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            transform.Rotate(Vector3.up * h * 120 * Time.deltaTime);
+            transform.Translate(Vector3.forward * v * 3 * Time.deltaTime);
+        }
+        else
+        {
+            tr.position = Vector3.Lerp(tr.position, currPos, Time.deltaTime * 3.0f);
+            tr.rotation = Quaternion.Lerp(tr.rotation, currRot, Time.deltaTime * 3.0f);
+        }
+        
+    }
+    private Vector3 currPos = Vector3.zero;
+    private Quaternion currRot = Quaternion.identity;
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.isWriting)
+        {
+            stream.SendNext(tr.position);
+            stream.SendNext(tr.rotation);
+        }
+        else
+        {
+            currPos = (Vector3)stream.ReceiveNext();
+            currRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
